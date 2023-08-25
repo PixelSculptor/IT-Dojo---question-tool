8b356e22c0205821456c7382d9c657327e9e4fd0
Kacper Nadstoga <kapix007@gmail.com>
2023-08-25 21:00:46 +0200

    refactor script to asking user for next step and adding next question to txt base on demand if question is not available in text

--- src/verifyQuestions.py
+++ src/verifyQuestions.py
@@ -6,14 +6,17 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
+QUESTION_BASE = './assets/test.txt'
+
 # Load NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
 # Preprocessing functions
+# Preprocessing functions
 def preprocess_text(text):
-    text = text.lower()
     text = re.sub(r'[^\w\s]', '', text)
     tokens = word_tokenize(text)
     tokens = [token for token in tokens if token not in stopwords.words('english')]
@@ -22,7 +25,7 @@ def preprocess_text(text):
     return ' '.join(tokens)
 
 # Load and preprocess the question base
-with open('./assets/test.txt', 'r') as file:
+with open(QUESTION_BASE, 'r') as file:
     question_base = file.readlines()
 
 question_base = [preprocess_text(question) for question in question_base]
@@ -31,23 +34,34 @@ question_base = [preprocess_text(question) for question in question_base]
 vectorizer = TfidfVectorizer()
 question_base_tfidf = vectorizer.fit_transform(question_base)
 
-# User input
-user_question = input("Please enter a question: ")
-user_question = preprocess_text(user_question)
+while True:
+    # User input
+    user_question = input("Please enter a question (or type 'exit' to quit): ")
 
-# TF-IDF vectorization for user question
-user_question_tfidf = vectorizer.transform([user_question])
+    if user_question.lower() == 'exit':
+        break
 
-# Calculate cosine similarities
-similarities = cosine_similarity(user_question_tfidf, question_base_tfidf).flatten()
+    user_question_preprocessed = preprocess_text(user_question)
+    user_question_tfidf = vectorizer.transform([user_question_preprocessed])
 
-# Find the most similar question
-most_similar_index = similarities.argmax()
-threshold = 0.4  # You can adjust this threshold as needed
+    # Calculate cosine similarities
+    similarities = cosine_similarity(user_question_tfidf, question_base_tfidf).flatten()
 
-if similarities[most_similar_index] > threshold:
-    matching_question = question_base[most_similar_index]
-    print("Similar question found in the base:")
-    print(matching_question)
-else:
-    print("No similar question found in the base.")
+    # Find the most similar question
+    most_similar_index = similarities.argmax()
+    threshold = 0.5  # You can adjust this threshold as needed
+
+    if similarities[most_similar_index] > threshold:
+        matching_question = question_base[most_similar_index]
+        print("Similar question found in the base:")
+        print(matching_question)
+    else:
+        print("No similar question found in the base.")
+        add_to_base = input("Do you want to add this question to the base? (yes/no): ")
+        if add_to_base.lower() == 'yes':
+            question_base.append(user_question_preprocessed)
+            with open(QUESTION_BASE, 'a') as file:
+                file.write(user_question + '\n')
+            print("Question added to the base.")
+
+print("Exiting the script.")
