8f284a2cfb4bf6c5b2964f9bbcb22b376391160d
Kacper Nadstoga <kapix007@gmail.com>
2023-08-29 21:19:38 +0200

    add feature with comparing questions on candidate list

--- src/verifyQuestions.py
+++ src/verifyQuestions.py
@@ -11,6 +11,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
+
 # Preprocessing functions
 def preprocess_text(text):
     text = re.sub(r'[^\w\s]', '', text)
@@ -19,6 +20,7 @@ def preprocess_text(text):
     lemmatizer = WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(token) for token in tokens]
     return ' '.join(tokens)
+
 
 # ask user for path:
 question_base_path = input("Please enter the path to the question base text file: ")
@@ -33,36 +35,41 @@ question_base = [preprocess_text(question) for question in question_base]
 vectorizer = TfidfVectorizer()
 question_base_tfidf = vectorizer.fit_transform(question_base)
 
-while True:
-    # User input
-    user_question = input("Please enter a question (or type 'exit' to quit): ")
 
-    if user_question.lower() == 'exit':
-        break
-
-    user_question_preprocessed = preprocess_text(user_question)
-    user_question_tfidf = vectorizer.transform([user_question_preprocessed])
+# Function to check similarity and add to base
+def check_similarity_and_add(candidate_question):
+    candidate_question_preprocessed = preprocess_text(candidate_question)
+    candidate_question_tfidf = vectorizer.transform([candidate_question_preprocessed])
 
     # Calculate cosine similarities
-    similarities = cosine_similarity(user_question_tfidf, question_base_tfidf).flatten()
+    similarities = cosine_similarity(candidate_question_tfidf, question_base_tfidf).flatten()
 
     # Find the most similar question
     most_similar_index = similarities.argmax()
-    threshold = 0.3  # You can adjust this threshold as needed
+    threshold = 0.5  # You can adjust this threshold as needed
 
     if similarities[most_similar_index] > threshold:
         matching_question = question_base[most_similar_index]
-        print("Similar question found in the base:")
-        print(matching_question)
+        print(f"Similar question found in the base for candidate: {candidate_question}")
+        print(f"Matching question: {matching_question}")
+        with open('./assets/errorlog.txt', 'a') as errorlog:
+            errorlog.write(candidate_question + '\n')
     else:
-        print("No similar question found in the base.")
+        print(f"No similar question found in the base for candidate: {candidate_question}")
         add_to_base = input("Do you want to add this question to the base? (yes/no): ")
         if add_to_base.lower() == 'yes':
-            question_base.append(user_question_preprocessed)
-            with open(question_base_path , 'a') as file:
-                file.write(user_question + '\n')
+            question_base.append(candidate_question_preprocessed)
+            with open(question_base_path, 'a') as file:
+                file.write(candidate_question + '\n')
             print("Question added to the base.")
 
+
+# Read and process candidate questions
+with open('./assets/candidates.txt', 'r') as candidates_file:
+    candidates = candidates_file.readlines()
+
+for candidate in candidates:
+    candidate = candidate.strip()
+    check_similarity_and_add(candidate)
+
 print("Exiting the script.")
-
-
